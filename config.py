365ff301ab40ad25636d93ca7db37e1c5237d5de
ricarfantinelli <45544590+ricarfantinelli@users.noreply.github.com>
2021-01-08 11:50:23 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config(object):
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'article-cms-serv.database.windows.net'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'article-cms-db'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'adm'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'rica*1234'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'rica@123'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
